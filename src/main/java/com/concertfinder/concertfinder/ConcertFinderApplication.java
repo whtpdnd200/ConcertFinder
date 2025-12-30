43f74285a660e824801abd3ad865a87740cf9d3e
whtpdnd200 <whtpdnd200@gmail.com>
2025-12-30 18:29:35 +0900

    [조세웅] - 프로젝트 생성 및 의존성 추가

---
+++ src/main/java/com/concertfinder/concertfinder/ConcertFinderApplication.java
@@ -0,0 +1,13 @@
+package com.concertfinder.concertfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertFinderApplication.class, args);
+    }
+
+}
