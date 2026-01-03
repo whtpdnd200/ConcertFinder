7525b43d52bec8cc63b407d973c7f0efaf1afe4f
whtpdnd200 <whtpdnd200@gmail.com>
2026-01-03 11:35:26 +0900

    [조세웅] - 공연 상세페이지 html 작업 완료 #7 - 공연 목록 리스트에 a 태그로 링크 달기 완료 #6

--- src/main/java/com/concertfinder/concertfinder/concert/ConcertController.java
+++ src/main/java/com/concertfinder/concertfinder/concert/ConcertController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,8 +39,8 @@ public class ConcertController {
         return "concertfinder/concert/list";
     }
 
-    @GetMapping("/detail")
-    public String detail() {
+    @GetMapping("/{id}")
+    public String detail(@PathVariable String id) {
         return "concertfinder/concert/detail";
     }
 }
