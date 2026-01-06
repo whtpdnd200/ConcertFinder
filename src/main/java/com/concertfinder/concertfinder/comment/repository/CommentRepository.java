7f0880eed3c82ad08ecdcacd59585eb091807883
whtpdnd200 <whtpdnd200@gmail.com>
2026-01-06 18:47:52 +0900

    [조세웅] - 공용으로 사용 할 ApiResponseDTO 생성

--- src/main/java/com/concertfinder/concertfinder/comment/repository/CommentRepository.java
+++ src/main/java/com/concertfinder/concertfinder/comment/repository/CommentRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    
+
 }
--- src/main/java/com/concertfinder/concertfinder/common/DTO/ApiResponseDTO.java
+++ src/main/java/com/concertfinder/concertfinder/common/DTO/ApiResponseDTO.java
@@ -13,7 +13,18 @@ public class ApiResponseDTO<T> {
 
     private T data;
 
+    // 게시글 댓글 목록 등 데이터가 존재하는 API 리턴시 사용
     public static <T>ApiResponseDTO<T> success(String message, T data) {
         return new ApiResponseDTO<>("success", message, data);
+    }
+
+    // 글 수정 삭제 등 데이터가 존재하지 않는 API 리턴시 사용
+    public static <T>ApiResponseDTO<T> success(String message) {
+        return new ApiResponseDTO<>("success", message,null);
+    }
+
+    // 에러가 발생 했을때 사용
+    public static <T>ApiResponseDTO<T> fail(String message) {
+        return new ApiResponseDTO<>("fail", message, null);
     }
 }
