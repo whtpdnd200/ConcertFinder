1a53dba2a832965c5ab5b48b187b49faf47b85f6
whtpdnd200 <whtpdnd200@gmail.com>
2026-01-06 23:03:39 +0900

    [조세웅] - 인터셉터 설정 클래스 생성 완료

---
+++ src/main/java/com/concertfinder/concertfinder/configuration/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.concertfinder.concertfinder.configuration;
+
+import com.concertfinder.concertfinder.interceptor.PermissionInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private PermissionInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/logout", "/css/**");
+
+    }
+
+}
