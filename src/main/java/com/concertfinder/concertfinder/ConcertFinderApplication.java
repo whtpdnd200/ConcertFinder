92ad5fef8493fa798d8bb7d6398002eda4cee19f
whtpdnd200 <whtpdnd200@gmail.com>
2026-02-09 16:06:17 +0900

    [조세웅] - 배포 설정 진행중

--- src/main/java/com/concertfinder/concertfinder/ConcertFinderApplication.java
+++ src/main/java/com/concertfinder/concertfinder/ConcertFinderApplication.java
@@ -2,6 +2,7 @@ package com.concertfinder.concertfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,7 +11,7 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @EnableScheduling
-public class ConcertFinderApplication {
+public class ConcertFinderApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConcertFinderApplication.class, args);
